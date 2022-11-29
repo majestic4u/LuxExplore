c1de90dce2abd9b973ab5133da5fb9b15703a489
majestic4u <sotiris.konstantinos@gmail.com>
2022-11-30 00:59:48 +0100

    added Swiper on WelcomeScreen 30/11/2022

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,15 +1,42 @@
 import React from "react";
 import { View, Image, Text, StyleSheet, TouchableOpacity } from "react-native";
+import Swiper from "react-native-swiper";
 
 export default function Welcomescreen() {
   return (
     <View style={{ flex: 1 }}>
-      <Image
-        style={styles.image}
-        source={{
-          uri: "https://images.unsplash.com/photo-1611745843519-2e03872876cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-        }}
-      />
+      <Swiper loop autoplay dotColor="grey" activeDotColor="#fff">
+        <Image
+          style={styles.image}
+          source={{
+            uri: "https://images.unsplash.com/photo-1564591419939-c067a954ec9d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+          }}
+        />
+        <Image
+          style={styles.image}
+          source={{
+            uri: "https://images.unsplash.com/photo-1611745843519-2e03872876cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+          }}
+        />
+        <Image
+          style={styles.image}
+          source={{
+            uri: "https://images.unsplash.com/photo-1590337318156-73e24cd1e36b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80://images.unsplash.com/photo-1611745843519-2e03872876cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+          }}
+        />
+        <Image
+          style={styles.image}
+          source={{
+            uri: "https://images.unsplash.com/photo-1588336899284-950764f07147?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+          }}
+        />
+        <Image
+          style={styles.image}
+          source={{
+            uri: "https://images.unsplash.com/photo-1590337318156-73e24cd1e36b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+          }}
+        />
+      </Swiper>
       <View style={styles.container}>
         <Text style={styles.header}>Explore Luxembourg City</Text>
         <Text style={styles.text}>
@@ -34,11 +61,11 @@ export default function Welcomescreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 0.4,
     backgroundColor: "#fff",
   },
   image: {
-    flex: 3,
+    flex: 1,
   },
   header: {
     fontSize: 26,
@@ -63,4 +90,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
     flex: 1,
   },
+  wrapper: {},
 });
